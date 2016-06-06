6be80ee4ba3fb9bcfc216709c96e563d797d119a
Piermaria Cosina <piermaria@belafonte.co>
2016-06-06 12:17:03 +0200

    new styling begin

---
+++ app/assets/js/home.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  var pepe = $.fn.fullpage({
+  verticalCentered: true,
+  slidesColor: ['#607D8B', '#607D8B', '#607D8B', '#607D8B', '#607D8B', '#607D8B', '#607D8B'],
+  anchors: ['home','campus', 'mobile', 'coding','video', 'aziende', 'contatti'],
+  menu: '#menu',
+  fixedElements: '#menu',
+  navigation:true,
+  navigationPosition: 'left',
+  slidesNavigation: true,
+  slidesNavPosition: 'bottom',
+  loopHorizontal: false,
+  touchSensitivity: 10,
+  scrollOverflow: false,
+  autoScrolling: false,
+  css3: true
+  });
+  //plugin function, place inside DOM ready function
+  outdatedBrowser({
+  bgColor: '#3f3f3f',
+  color: '#fff',
+  lowerThan: 'IE10'
+  });
+
+  $("#close").click(function(){
+    $("#popup").fadeOut();
+    $(".topintro").css( "margin-top", "10%");
+  });
+
+  $( "#iphone_menu" ).click(function() {
+    event.preventDefault();
+    $( ".iphone_menu" ).toggle( "slow" );
+    if ($(this).text() =="MENU"){
+      $(this).text("CHIUDI")
+    }else{
+      $(this).text("MENU");
+    }
+  });
+});
+
+$(window).load(function() { // makes sure the whole site is loaded
+  $('#status').fadeOut(); // will first fade out the loading animation
+  $('#preloader').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(350).css({'overflow':'visible'});
+})
