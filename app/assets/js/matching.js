b3eadac483b6e2156365dddc41bbc916b203070e
Piermaria Cosina <piermaria.cosina@gmail.com>
2016-06-20 10:21:48 +0200

    filtri matching quasi finiti

--- app/assets/js/matching.js
+++ app/assets/js/matching.js
@@ -4,33 +4,31 @@ var studentCounter = 0;
 var totalStudents = 0;
 var studentsList;
 
-function getTemplate(studentsList){
+function getStudentsTemplate(studentsList){
   var source   = $("#student-template").html();
   var template = Handlebars.compile(source);
-  console.log(studentsList);
+
   var html= template({student:studentsList});
   $('#students').append(html);
 
 }
 
-function eachStudent(students){
-  studentsList = students;
+function getYear(index,student, selectedYear){
 
-  getTemplate(studentsList);
+  var Years = Parse.Object.extend('Year');
+  var yearQuery = new Parse.Query(Years);
+  yearQuery.equalTo('year', student);
+  yearQuery.descending('createdAt');
+  yearQuery.find({
+    success: function(year) {
+        studentsList[index].year = year;
 
-  totalStudents = students.length;
-  console.log("totalStudents "+totalStudents);
-}
+        studentCounter += 1;
 
-function getStudents(){
-  var Student = Parse.Object.extend("Student");
-
-  var query = new Parse.Query(Student);
-  // query.equalTo("active", status);
-  query.descending("createdAt");
-  query.find({
-    success: function(students) {
-      eachStudent(students);
+        if(studentCounter == totalStudents){
+            getStudentsTemplate(studentsList);
+        }
+        console.log("studentCounter "+studentCounter);
 
     },
     error: function(error) {
@@ -39,7 +37,73 @@ function getStudents(){
   });
 }
 
-getStudents();
+function eachStudent(students,selectedYear){
+
+  $( students ).each(function( index,student ) {
+    getYear(index,student,selectedYear);
+  });
+  studentsList = students;
+
+
+  totalStudents = students.length;
+  console.log("totalStudents "+totalStudents);
+}
+
+function getStudents(courses,year){
+
+  var Student = Parse.Object.extend("Student");
+
+  var query = new Parse.Query(Student);
+  console.log(courses);
+  query.containedIn("course", courses);
+
+  query.descending("createdAt");
+  query.find({
+    success: function(students) {
+      eachStudent(students,year);
+
+    },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+}
+
+var yearCounter = 0;
+var totalYears = 0;
+var yearsList;
+
+function getTemplate(yearsList){
+  var source   = $("#year-template").html();
+  var template = Handlebars.compile(source);
+  // console.log(yearsList);
+  var html= template({year:yearsList});
+  $('#filter').prepend(html);
+}
+
+
+
+function getYears(){
+  var Year = Parse.Object.extend("Year");
+
+  var query = new Parse.Query(Year);
+  // query.equalTo("active", status);
+  query.find({
+    success: function(years) {
+      yearsList = years;
+      getTemplate(yearsList);
+
+      totalYears = years.length;
+      console.log("totalYears "+totalYears);
+
+    },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+}
+getYears();
+
 
 
 $(document).ready(function(){
@@ -60,12 +124,12 @@ $(document).ready(function(){
     });
 
 
-    $("#visiona").click(function(e) {
-      e.preventDefault();
-      $('html, body').animate({
-          scrollTop: $(".career-03-wrapper").offset().top
-      }, 1000);
-    });
+    // $("#visiona").click(function(e) {
+    //   e.preventDefault();
+    //   $('html, body').animate({
+    //       scrollTop: $(".career-03-wrapper").offset().top
+    //   }, 1000);
+    // });
 
 
     $( "#iphone_menu" ).click(function() {
@@ -86,9 +150,41 @@ $(document).ready(function(){
         resetForm: true
         // target:        '#output1',
     };
+    var options2 = {
+        beforeSubmit:  showRequest2,
+        error: showError
+        // target:        '#output1',
+    };
 
     $('#matching-form').ajaxForm(options);
+    $('#filter').ajaxForm(options2);
 });
+function cleanStudents(){
+  studentCounter = 0;
+  totalStudents = 0;
+
+  $("#students").empty();
+}
+
+function showRequest2(formData, jqForm, options) {
+    cleanStudents();
+    var queryString = $.param(formData);
+    var courses=[];
+    var year= formData[0].value;
+
+    if(formData[2]){
+      courses.push(formData[2].value);
+    }
+    if(formData[3]){
+      courses.push(formData[3].value);
+    }
+    if(formData[4]){
+      courses.push(formData[4].value);
+    }
+
+    getStudents(courses,year);
+    return true;
+}
 
 // pre-submit callback
 function showRequest(formData, jqForm, options) {
